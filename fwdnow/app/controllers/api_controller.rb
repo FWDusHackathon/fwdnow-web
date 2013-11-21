e58ba72442d88bc7166686773f4fade4df591c2c
Alexey Komissarouk <alexey@alexeymk.com>
2013-11-21 02:06:58 -0800

    rails api endpoint for sincerely works

--- fwdnow/app/controllers/api_controller.rb
+++ fwdnow/app/controllers/api_controller.rb
@@ -1,15 +1,36 @@
-require 'net/http'
+require 'rest-client'
+require 'json'
 
 class ApiController < ApplicationController
   def postcard
-    sincerely_args = params.clone
-    sincerely_args['appkey'] = 'JM1WE0MPZPS6BETVQ94DH2VFUSKUHW4IXC1OL7AS'
-    response = Net::HTTP.post_form
-      URI.parse('https://snapi.sincerely.com/shiplib/create'),
-      sincerely_args
-    # AMK NEXT: todo actually pass params thru
-    puts response.code, response.body
+    url = 'https://snapi.sincerely.com/shiplib/create'
+    request = {
+      :appkey => 'JM1WE0MPZPS6BETVQ94DH2VFUSKUHW4IXC1OL7AS',
+      :testMode => true,
+      :frontPhotoUrl => params[:photo_url],
+      :message => params[:message],
+      :recipients => [ # TODO - from params[:zipcode]
+         {
+          :name => "Jane Doe",
+          :street1 => "123 Mission Street",
+          :city => "San Francisco",
+          :state => "CA",
+          :postalcode => "94105",
+          :country => "UNITED STATES"
+          }
+         ].to_json,
+      :sender => { # TODO maybe from constituent?
+          :name => "Pepper Gram",
+          :email => "pepper@sincerely.com",
+          :street1 => "800 Market Street",
+          :city => "San Francisco",
+          :state => "CA",
+          :postalcode => "94105",
+          :country => "UNITED STATES"
+        }.to_json
+    }
 
-    return response
+    resp = RestClient.post(url,request,:accept=>:json)
+    render :json => resp
   end
 end
--- fwdnow/config/routes.rb
+++ fwdnow/config/routes.rb
@@ -1,8 +1,7 @@
 Fwdnow::Application.routes.draw do
   resources :celebrities, :path => '/', :except => [:show, :destroy]
   get '/:id' => 'celebrities#show'
-
-  get 'api/send_postcard' => 'api#postcard'
+  post '/api/send_postcard' => 'api#postcard'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
