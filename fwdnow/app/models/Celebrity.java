d22bd2ff6f72659a36fb87606b3b6d9a1a56dcc0
Nipun Dave <ndave@ndave-mn1.linkedin.biz>
2013-11-20 22:35:21 -0800

    Initial commit of all files

---
+++ fwdnow/app/models/Celebrity.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ndave
+ * Date: 11/20/13
+ * Time: 9:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Celebrity {
+}
